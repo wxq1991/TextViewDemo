46915216a9f6d36d1b6149d72cc917d3536ac0d9
wxq <3302436132@qq.com>
2017-10-16 10:29:30 +0800

    点击修改TextView字体颜色

--- app/src/main/java/com/example/administrator/textviewdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/textviewdemo/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.administrator.textviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTv;
+    private Boolean mSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTv = (TextView) findViewById(R.id.tv);
+
+        mTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mSelected) {
+                    mSelected = true;
+                    mTv.setSelected(true);
+                }else {
+                    mSelected = false;
+                    mTv.setSelected(false);
+                }
+            }
+        });
+
+
     }
 }
